b9b1afc3f4c9c888c63279ee8b28cfea334e2112
shiri24 <shiri24@ajou.ac.kr>
2022-04-26 19:02:04 +0900

    edit UI
    edit UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
+
+        //위치 권한 요청 - 송시은
+        /*if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Pickingroad");
     }
 }
