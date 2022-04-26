6cbce8ebff9477c7ad02ef60d5dbd5f2c7b79338
shiri24 <shiri24@ajou.ac.kr>
2022-04-26 19:30:36 +0900

    Test_add Inventory System
    Test_add Inventory System

---
+++ Assets/InventorySystem/Scripts/SC_PickItem.cs
@@ -0,0 +1,21 @@
+﻿//You are free to use this script in Free or Commercial projects
+//sharpcoderblog.com @2019
+
+using UnityEngine;
+
+public class SC_PickItem : MonoBehaviour
+{
+    public string itemName = "Some Item"; //Each item must have an unique name
+    public Texture itemPreview;
+
+    void Start()
+    {
+        //Change item tag to Respawn to detect when we look at it
+        gameObject.tag = "Respawn";
+    }
+
+    public void PickItem()
+    {
+        Destroy(gameObject);
+    }
+}
